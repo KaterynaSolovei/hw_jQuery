04b4549dcbe012a7d79e6e3823d3d73cd2d1b0b3
Kateryna Solovei <soloveikat07@gmail.com>
2023-04-20 22:11:27 -0400

    basic done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,51 +8,16 @@ const btnClickHandler = (e) => {
   const target = e.target;
   const interval = 2000;
 
-  if (typeof target !== 'object') return;
-  if (!target.matches('.item-actions__cart')) return;
-  incrementCounter(cartCounterLabel,++cartCounter);
-
-  const mock = +target.parentElement.previousElementSibling.innerHTML
-  .replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
-
-  cartPrice = Math.round((cartPrice + mock) *100) / 100;
-
-  target.innerHTML = `Added ${cartPrice.toFixed(2)}$`;
-
-  setTimeout(() => {
-target.innerHTML ='return'
-  }, interval);
-};
-
-contentContainer.addEventListener('click', btnClickHandler);
-
-function incrementCounter($label, cn) {
- $label.innerText = cn;
-  if (cn === 1) $label.style.display = 'block';
-}
-
-
-/*onst contentContainer = document.querySelector('#content-container');
-const cartCounterLabel = document.querySelector('#cart-counter-label');
-
-let cartCounter = 0;
-let cartPrice = 0;
-
-const btnClickHandler = (e) => {
-  const target = e.target;
-  const interval = 2000;
-
   let restoreHTML = null;
 
   if (typeof target !== 'object') return;
   if (!target.matches('.item-actions__cart')) return;
-
+  
   incrementCounter(cartCounterLabel, ++cartCounter);
   cartPrice = getPrice(target, cartPrice, getMockData);
 
   restoreHTML = target.innerHTML;
-  target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
-
+  target.innerHTML = `Added ${cartPrice.toFixed(2)}$`;
   disableControls(target, contentContainer, btnClickHandler);
 
   setTimeout(() => {
@@ -64,19 +29,19 @@ const btnClickHandler = (e) => {
 contentContainer.addEventListener('click', btnClickHandler);
 
 function incrementCounter($label, cn) {
-  $label.innerHTML = `${cn}`;
+  $label.innerText = cn;
   if (cn === 1) $label.style.display = 'block';
 }
 
 function getMockData(t) {
   return +t.parentElement
-      .previousElementSibling
-      .innerHTML
-      .replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
+    .previousElementSibling
+    .innerHTML
+    .replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
 }
 
 function getPrice(t, p, cb) {
-  return Math.round((p + cb(t)) * 100) / 100;
+  return cartPrice = Math.round((p + cb(t)) * 100) / 100;
 }
 
 function disableControls(t, $el, fn) {
@@ -88,4 +53,3 @@ function enableControls(t, $el, fn) {
   t.disabled = false;
   $el.addEventListener('click', fn);
 }
-*/
